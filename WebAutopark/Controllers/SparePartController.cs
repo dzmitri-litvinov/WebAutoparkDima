ae1fcaf82c3317eeb2b6934a4d94bb3f4d28edd0
dzmitri-litvinov <79978383+dzmitri-litvinov@users.noreply.github.com>
2021-06-03 22:18:54 +0300

    WebAutoParkWeek3 (#4)
    * WebAutoParkWeek3
    
    Add functionality for SpareParts and VehicleTypes
    
    * Update Index.cshtml
    
    * Remove Buttons
    
    * Create forms using helpers

---
+++ WebAutopark/Controllers/SparePartController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAutopark.DAL.Interfaces;
+using WebAutopark.DAL.Entities;
+
+namespace WebAutopark.Controllers
+{
+    public class SparePartController : Controller
+    {
+        private readonly IRepository<SparePart> _sparePartRepository;
+
+        public SparePartController(IRepository<SparePart> sparePartRepository)
+        {
+            _sparePartRepository = sparePartRepository;
+        }
+        public IActionResult Index()
+        {
+            var spareParts = _sparePartRepository.GetAll();
+            spareParts = spareParts.OrderBy(spareParts => spareParts.Id);
+            return View(spareParts);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            _sparePartRepository.Delete(id);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Update(int id)
+        {
+            var sparePart = _sparePartRepository.GetById(id);
+            return View(sparePart);
+        }
+
+        [HttpPost]
+        public IActionResult Update(SparePart sparePart)
+        {
+            _sparePartRepository.Update(sparePart);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(SparePart sparePart)
+        {
+            _sparePartRepository.Create(sparePart);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ WebAutopark/Controllers/VehicleTypeController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAutopark.DAL.Interfaces;
+using WebAutopark.DAL.Entities;
+
+namespace WebAutopark.Controllers
+{
+    public class VehicleTypeController : Controller
+    {
+        private readonly IRepository<VehicleType> _vehicleTypeRepository;
+
+        public VehicleTypeController(IRepository<VehicleType> vehicleTypeRepository)
+        {
+            _vehicleTypeRepository = vehicleTypeRepository;
+        }
+        public IActionResult Index()
+        {
+            var vehicleTypes = _vehicleTypeRepository.GetAll();
+            vehicleTypes = vehicleTypes.OrderBy(vehicleTypes => vehicleTypes.Id);
+            return View(vehicleTypes);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            _vehicleTypeRepository.Delete(id);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Update(int id)
+        {
+            var vehicleType = _vehicleTypeRepository.GetById(id);
+            return View(vehicleType);
+        }
+
+        [HttpPost]
+        public IActionResult Update(VehicleType vehicleType)
+        {
+            _vehicleTypeRepository.Update(vehicleType);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(VehicleType vehicleType)
+        {
+            _vehicleTypeRepository.Create(vehicleType);
+            return RedirectToAction("Index");
+        }
+    }
+}
